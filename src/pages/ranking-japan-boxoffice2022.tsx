87c05116decb3bf8785c8c39894796bc9b204fc7
nawawa <oonawa12@gmail.com>
2023-02-22 13:24:35 +0900

    feat: TypeScriptの型指定を入れて配列を表示する処理を追加

--- src/pages/ranking-japan-boxoffice2022.tsx
+++ src/pages/ranking-japan-boxoffice2022.tsx
@@ -1,3 +1,32 @@
-export default function RankingJapanBoxoffice2022() {
-  return <div>ranking-japan-boxoffice2022</div>
+import { NextPage } from "next"
+
+interface MovieData {
+  title: string
 }
+
+const movies: MovieData[] = [
+  { title: '作品1' },
+  { title: '作品2' },
+  { title: '作品3' },
+  { title: '作品4' },
+  { title: '作品5' },
+  { title: '作品6' },
+  { title: '作品7' },
+  { title: '作品8' },
+]
+
+const rankingJapanBoxoffice2022: NextPage = () => {
+  return (
+    <>
+      <div>
+        <ol>
+          {
+            movies.map(movie => <li>{movie.title}</li>)
+          }
+        </ol>
+      </div>
+    </>
+  )
+}
+
+export default rankingJapanBoxoffice2022
