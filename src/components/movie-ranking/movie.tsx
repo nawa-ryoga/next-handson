82b4116a15eb0b146f00072c17c50b859f8d4a3d
nawawa <oonawa12@gmail.com>
2023-02-27 14:08:11 +0900

    refactor: 不要になったViewの出し分け用カスタムフックを削除・Viewの出し分けを廃止

--- src/components/movie-ranking/movie.tsx
+++ src/components/movie-ranking/movie.tsx
@@ -1,8 +1,6 @@
 import { css } from "@emotion/react";
 import { Movie, MovieRevenue } from "../../types/movie";
 
-import {useClient} from "../../hooks/useClient";
-
 interface Props {
   movie: Movie,
   revenue: MovieRevenue
@@ -16,83 +14,75 @@ const backgroundStyle = (backdrop_path?: string): string => {
 }
 
 const MovieCard = ({ movie, revenue }: Props) => {
-  const isClient = useClient()
-
   return (
-    <>
-      {
-        isClient ? 
-          <div
+    <div
+      css={css`
+        padding: 12px 24px;
+        max-width: 60%;
+        margin: 0 auto;
+      `}
+    >
+      <div
+        css={css`
+          width: 100%;
+          height: auto;
+          padding: 16px;
+          border-radius: 4px;
+          display: flex;
+          ${backgroundStyle(movie.backdrop_path)};
+        `}
+      >
+        <div
+          css={css`
+            flex: 1;
+            padding-right: 16px;
+          `}
+        >
+          <h3
             css={css`
-              padding: 12px 24px;
-              max-width: 60%;
-              margin: 0 auto;
+              font-size: 1.5rem;
             `}
           >
-            <div
-              css={css`
-                width: 100%;
-                height: auto;
-                padding: 16px;
-                border-radius: 4px;
-                display: flex;
-                ${backgroundStyle(movie.backdrop_path)};
-              `}
+            {movie.title}
+          </h3>
+          <h4
+            css={css`
+              font-size: 1.1rem;
+              padding: 4px 0;
+            `}
             >
-              <div
-                css={css`
-                  flex: 1;
-                  padding-right: 16px;
-                `}
-              >
-                <h3
-                  css={css`
-                    font-size: 1.5rem;
-                  `}
-                >
-                  {movie.title}
-                </h3>
-                <h4
-                  css={css`
-                    font-size: 1.1rem;
-                    padding: 4px 0;
-                  `}
-                  >
-                    {`
-                      興行収入：${revenue}億円
-                    `}
-                  </h4>
-                <p
-                  css={css`
-                    font-size: 14px;
-                    line-height: 1.5;
-                    opacity: 0.8;
-                    margin-top: 16px;
-                  `}
-                >
-                  {movie.overview}
-                </p>
-              </div>
-              <div
-                css={css`
-                  max-width: 150px;
-                  display:flex;
-                  align-items: center;
-                `}
-              >
-                <img 
-                  src={`${process.env.NEXT_PUBLIC_TMDB_IMG_PATH}/${movie.poster_path}`} 
-                  alt={`${movie.title}のポスター`} 
-                  css={css`
-                    max-width: 100%;
-                  `}
-                />
-              </div>
-            </div>
-          </div>:
-          <></>
-      }
-    </>
+              {`
+                興行収入：${revenue}億円
+              `}
+            </h4>
+          <p
+            css={css`
+              font-size: 14px;
+              line-height: 1.5;
+              opacity: 0.8;
+              margin-top: 16px;
+            `}
+          >
+            {movie.overview}
+          </p>
+        </div>
+        <div
+          css={css`
+            max-width: 150px;
+            display:flex;
+            align-items: center;
+          `}
+        >
+          <img 
+            src={`${process.env.NEXT_PUBLIC_TMDB_IMG_PATH}/${movie.poster_path}`} 
+            alt={`${movie.title}のポスター`} 
+            css={css`
+              max-width: 100%;
+            `}
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
