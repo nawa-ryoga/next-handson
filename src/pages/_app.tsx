1f716274181e0c887658fafd5a03e3e112a34872
nawawa <oonawa12@gmail.com>
2023-02-28 12:28:43 +0900

    feat: ダークモードに設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,19 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 
+const theme = {
+  config: {
+    initialColorMode: 'dark',
+    useSystemColorMode: false,
+  }
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider
+      theme={extendTheme(theme)}
+    >
       <Component {...pageProps} />
     </ChakraProvider>
   )
