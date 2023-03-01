c0c0fc0646c2b768c89c6f790739726b6094d338
nawawa <oonawa12@gmail.com>
2023-03-01 15:23:33 +0900

    ref: 映画カードの ChakraUI コンポーネントを Box から Card へ変更

--- src/components/movie-ranking/movie.tsx
+++ src/components/movie-ranking/movie.tsx
@@ -6,6 +6,9 @@ import {
   Text,
   Image,
   Flex, 
+  Card,
+  CardHeader,
+  CardBody
 } from '@chakra-ui/react'
 
 interface Props {
@@ -22,60 +25,67 @@ const backgroundStyle = (backdrop_path?: string): string => {
 
 const MovieCard = ({ movie, revenue }: Props) => {
   return (
-    <Box
-      px="3"
-      py="6"
+    <Card
       maxW="60%"
       mx="auto"
+      bg={backgroundStyle(movie.backdrop_path)}
     >
       <Flex
         w="100%"
         h="auto"
-        p="4"
         borderRadius="4"
-        bg={backgroundStyle(movie.backdrop_path)}
       >
         <Box
           pr="4"
         >
-          <Heading
-            as="h3"
-            size="md"
-            fontSize="1.5rem"
+          <CardHeader>
+            <Heading
+              as="h3"
+              size="md"
+              fontSize="1.5rem"
+            >
+              {movie.title}
+            </Heading>
+            <Heading
+              as="h4"
+              size="sm"
+              fontSize="1.1rem"
+              py="1"
+            >
+              {`
+                興行収入：${revenue}億円
+              `}
+            </Heading>
+          </CardHeader>
+          <CardBody
+            pt={0}
           >
-            {movie.title}
-          </Heading>
-          <Heading
-            as="h4"
-            size="sm"
-            fontSize="1.1rem"
-            py="1"
-          >
-            {`
-              興行収入：${revenue}億円
-            `}
-          </Heading>
-          <Text
-            fontSize="14px"
-            lineHeight="1.5"
-            opacity="0.8"
-            mt="4"
-          >
-            {movie.overview}
-          </Text>
+            <Text
+              fontSize="14px"
+              lineHeight="1.5"
+              opacity="0.8"
+            >
+              {movie.overview}
+            </Text>
+          </CardBody>
         </Box>
         <Flex
           maxW="150px"
           align="center"
         >
-          <Image 
-            src={`${process.env.NEXT_PUBLIC_TMDB_IMG_PATH}/${movie.poster_path}`} 
-            alt={`${movie.title}のポスター`} 
-            maxW="100%"
-          />
+          <CardBody 
+            py={0}
+            pl={0}
+          >
+            <Image 
+              src={`${process.env.NEXT_PUBLIC_TMDB_IMG_PATH}/${movie.poster_path}`} 
+              alt={`${movie.title}のポスター`} 
+              maxW="100%"
+            />
+          </CardBody>
         </Flex>
       </Flex>
-    </Box>
+    </Card>
   )
 }
 
