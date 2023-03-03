d7b0148270c38cbdb4d168b157b3c8cad9cc5943
nawawa <oonawa12@gmail.com>
2023-03-03 12:52:16 +0900

    ref: API へのアクセス処理を  src/script/ へ外部化

--- src/pages/api/movies.ts
+++ src/pages/api/movies.ts
@@ -1,18 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import type { Movie, MovieId, MovieRevenue } from "types/movie";
-import movieIdRevenueMaps from 'consts/movie-Id-revenue-map'
+import type { Movie } from "types/movie";
 
-const movieIdList = (index: number): MovieId[] => {
-  return Array.from(movieIdRevenueMaps[index].keys())
-}
-
-const getMovieData = (index: number): Promise<Movie>[] => {
-  return movieIdList(index).map((id) => {
-    return fetch(
-      `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.TMDB_API_KEY}&language=ja`
-    ).then(res => res.json())
-  })
-}
+import { getMovieData } from "script/get-movie"
 
 const handler = async (req: NextApiRequest, res: NextApiResponse<Movie[]>) => {
   const indexNumber = Number(req.query.page) - 1
